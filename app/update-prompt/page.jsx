91e8a2a4dc600e51b662a55a19a8acb198f6d921
Amine Sakouhi <sakouhiamine04@gmail.com>
2024-07-23 15:28:52 +0100

    wrapped the form in update-prompt with suspense tag

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
@@ -50,13 +50,15 @@ const EditPrompt = () => {
   };
 
   return (
-    <Form
-      type="Edit"
-      post={post}
-      setpost={setpost}
-      submitting={submitting}
-      handleSubmit={updatePrompt}
-    />
+    <Suspense>
+      <Form
+        type="Edit"
+        post={post}
+        setpost={setpost}
+        submitting={submitting}
+        handleSubmit={updatePrompt}
+      />
+    </Suspense>
   );
 };
 
